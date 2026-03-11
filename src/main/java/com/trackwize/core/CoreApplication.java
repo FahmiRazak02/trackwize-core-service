694d8fbd703e8b16aaeb15ba92edc52a9e0bbb2a
aiman.fahmi <aiman.fahmi@beans.com.my>
2026-03-11 14:35:54 +0800

    update config

--- src/main/java/com/trackwize/core/CoreApplication.java
+++ src/main/java/com/trackwize/core/CoreApplication.java
@@ -2,8 +2,13 @@ package com.trackwize.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.trackwize.core",
+        "com.trackwize.common"
+})
 public class CoreApplication {
 
 	public static void main(String[] args) {
